b0dba7e1f1daa40eea1e39a40f7e97fa8835a856
Derlis Canete <derliscrdev@gmail.com>
2024-03-14 00:13:57 -0300

    Modify models, urls and views from Usuers

--- GroupTours/apps/usuario/models.py
+++ GroupTours/apps/usuario/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
+class Usuario(models.Model):
     '''
     Abstraccion de la clase Usuario
     '''
-    document = models.CharField(max_length=8)
-    name = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-    phoneNumber = models.CharField(max_length=15)
-    email = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    status = models.CharField(max_length=1, default='')
-    form = models.CharField(max_length=50, default='')
+    documento = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    correo = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    estado = models.CharField(max_length=1, default='')
+    formulario = models.CharField(max_length=50, default='')
     
-    #personalizamos la tabla en posgres
+    #personalizamos la tabla en postgres
     class Meta:
         verbose_name = 'Usuario'
         db_table = 'Usuario'
@@ -23,11 +23,11 @@ class Users(models.Model):
     
     def _str_(self):
         texto = '{} {} {}'
-        return texto.format(self.document,
-                            self.name, 
-                            self.lastName,
-                            self.phoneNumber,
-                            self.email,
-                            self.address,
-                            self.status,
+        return texto.format(self.documento,
+                            self.nombre, 
+                            self.apellido,
+                            self.telefono,
+                            self.correo,
+                            self.direccion,
+                            self.estado,
                             self.formulario) 
--- GroupTours/apps/usuario/views.py
+++ GroupTours/apps/usuario/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from django.http import HttpResponse
 from .models import Usuario
-from .serializers import UsuarioSerializer
+# Create your views here.
 
-class UsuarioList(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
-    
-    
-    # def index(request):
-    #     return render(request, "usuarios.html")
+def index(request):
+    # print(request.path)
+    listaUsuario = Usuario.objects.all().order_by('id')
+    return render(request, 'usuario.html', {'listaUsuarios':listaUsuario})
+
+
+def registrarUsuario(request):
+    #se recupera los datos del formulario
+    documento = request.POST.get('txtDocumento')
+    nombre = request.POST.get('txtNombre')
+    telefono = request.POST.get('txtTelefono')
+    correo = request.POST.get('txtCorreo')
+    direccion = request.POST.get('txtDireccion')
+    estado = request.POST.get('txtEstado')
+    formulario = request.POST.get('txtFormulario')
+   
+    #se crea un permiso
+    Usuario.objects.create(documento = documento,
+                           nombre = nombre,
+                           telefono = telefono,
+                           correo = correo,
+                           direccion = direccion,
+                           estado = estado,
+                           formulario = formulario)
+
+    #se recupera toda la lista de permiso
+    listaUsuario = Usuario.objects.all().order_by('id')
+
+    return render(request, 'usuario.html', {'listaUsuarios':listaUsuario} )
+
+def eliminar(request, id):
+    try:
+        usuario = Usuario.objects.get(id=id)
+        usuario.delete()
+    except:
+        pass
+
+    listaUsuario = Usuario.objects.all().order_by('id')
+
+    return render(request, 'usuario.html', {'listaUsuarios':listaUsuario})
