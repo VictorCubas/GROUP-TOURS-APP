a2e90e402257f0ddf5494ef3129d405dc2547564
escurracaceres <escurracaceres.andy@fpuna.edu.py>
2024-03-16 19:21:50 -0300

    se agrega la funcionalidad editar

--- GroupTours/apps/permiso/urls.py
+++ GroupTours/apps/permiso/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index-permiso'),
     path('/registrarPermiso', views.registrarPermiso, name='registrar-permiso'),
-    path('/edicionPermiso/<codigo>', views.edicionPermiso, name='editar-permiso'),
+    path('/edicionPermiso/<int:codigo>', views.edicionPermiso, name='edicion-permiso'),
+    path('/editarPermiso/<int:id>', views.editarPermiso, name='editar-permiso'),
     path('/eliminar/<int:id>', views.eliminar, name='eliminar-permiso'),
     
 ]
--- GroupTours/apps/permiso/views.py
+++ GroupTours/apps/permiso/views.py
@@ -25,8 +25,28 @@ def registrarPermiso(request):
     return render(request, 'permiso.html',{'listaPermisos':listaPermiso} )
 
 def edicionPermiso(request, codigo):
-    permiso = Permiso.objects.get(id=id)
+    permiso = Permiso.objects.get(id=int(codigo))
     return render(request, "edicionPermiso.html", {"permiso": permiso })
+
+def editarPermiso(request, id):
+    nombre = request.POST.get('txtNombre')
+    descripcion = request.POST.get('txtDescripcion')
+    tipo = request.POST.get('txtTipo')
+    formulario = request.POST.get('txtFormulario')
+
+    try:
+        permiso = Permiso.objects.get(id=id)
+        permiso.nombre = nombre
+        permiso.descripcion = descripcion
+        permiso.tipo = tipo
+        permiso.formulario = formulario
+        permiso.save()
+
+    except:
+        pass
+    
+    listaPermiso = Permiso.objects.all().order_by('id')
+    return render(request, 'permiso.html',{'listaPermisos':listaPermiso} )
 
 def eliminar(request, id):
     try:
