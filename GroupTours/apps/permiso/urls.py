74da28645aba13e9be4297be85bedd8247e51530
Derlis Canete <derliscrdev@gmail.com>
2024-03-18 19:16:11 -0300

    fixes into permiso

--- GroupTours/apps/permiso/urls.py
+++ GroupTours/apps/permiso/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('/registrarPermiso', views.registrarPermiso, name='registrar-permiso'),
     path('/edicionPermiso/<int:codigo>', views.edicionPermiso, name='edicion-permiso'),
     path('/editarPermiso/<int:id>', views.editarPermiso, name='editar-permiso'),
-    path('/eliminar/<int:id>', views.eliminar, name='eliminar-permiso'),
-    
+    path('/eliminar/<int:id>', views.eliminar, name='eliminar-permiso'),    
 ]
