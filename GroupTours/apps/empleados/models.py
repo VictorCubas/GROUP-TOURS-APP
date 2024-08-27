d37daf40bb43f50407c596510a7c99e8e6c4361c
Victor Cubas <vhcubas91@gmail.com>
2024-08-27 19:54:30 -0400

    Se agrega la clase Persona para que Empleado herede del mismo

--- GroupTours/apps/empleados/models.py
+++ GroupTours/apps/empleados/models.py
@@ -2,16 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class Empleado(models.Model):
+class Persona(models.Model):
+    '''
+    Abstraccion de la clase Persona
+    '''
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=50, default='')
+    fecha_nacimiento = models.DateTimeField()
+    class Meta:
+        abstract = True
+
+class Empleado(Persona):
     '''
     Abstraccion de la clase Empleado
     '''
-    
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200, default='')
     tipo = models.CharField(max_length=1, default='')
-    formulario = models.CharField(max_length=50, default='')
     activo = models.BooleanField(default=True)
     
     #personalizamos la tabla en postgre
