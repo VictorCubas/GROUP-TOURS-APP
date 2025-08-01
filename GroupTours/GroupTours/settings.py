cb293ea069311ae21f228028daca300e634a2e72
Victor Cubas <vhcubas91@gmail.com>
2025-08-01 07:24:53 -0300

    [feat] config para el servidor

--- GroupTours/GroupTours/settings.py
+++ GroupTours/GroupTours/settings.py
@@ -27,12 +27,17 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)@hy_fb457w++0#f6i$$qm*3a-j=s8)kp5rjrb#(lifpmg_&3'
+# SECRET_KEY = 'django-insecure-o)@hy_fb457w++0#f6i$$qm*3a-j=s8)kp5rjrb#(lifpmg_&3'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 CORS_ALLOW_ALL_ORIGINS = True
 
