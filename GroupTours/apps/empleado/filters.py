b63b93041daeb5e87f6fa2be7a4677e8dce70065
Victor Cubas <vhcubas91@gmail.com>
2025-08-23 22:38:46 -0300

    [feat] se agrega filtros por documentos, nombre, apellito y razon social

--- GroupTours/apps/empleado/filters.py
+++ GroupTours/apps/empleado/filters.py
@@ -3,10 +3,40 @@ from django_filters.rest_framework import FilterSet
 from .models import Empleado
 
 class EmpleadoFilter(FilterSet):
-    persona = django_filters.CharFilter(field_name='persona__documento', lookup_expr='icontains')
-    puesto = django_filters.CharFilter(field_name='puesto__nombre', lookup_expr='icontains')
-    activo = django_filters.BooleanFilter(field_name='activo')
+    persona = django_filters.CharFilter(
+        field_name='persona__documento',
+        lookup_expr='icontains'
+    )
+    puesto = django_filters.CharFilter(
+        field_name='puesto__nombre',
+        lookup_expr='icontains'
+    )
+    activo = django_filters.BooleanFilter(
+        field_name='activo'
+    )
+
+    # 📅 Filtros de rango para fecha_ingreso
+    fecha_ingreso_desde = django_filters.DateFilter(
+        field_name='fecha_ingreso',
+        lookup_expr='gte'
+    )
+    fecha_ingreso_hasta = django_filters.DateFilter(
+        field_name='fecha_ingreso',
+        lookup_expr='lte'
+    )
+    
+    # 🔹 Filtros por datos de persona
+    documento = django_filters.CharFilter(field_name='persona__documento', lookup_expr='icontains')
+    nombre = django_filters.CharFilter(field_name='persona__personafisica__nombre', lookup_expr='icontains')
+    apellido = django_filters.CharFilter(field_name='persona__personafisica__apellido', lookup_expr='icontains')
+    razon_social = django_filters.CharFilter(field_name='persona__personajuridica__razon_social', lookup_expr='icontains')
 
     class Meta:
         model = Empleado
-        fields = ['persona', 'puesto', 'activo']
+        fields = [
+            'persona',
+            'puesto',
+            'activo',
+            'fecha_ingreso_desde',
+            'fecha_ingreso_hasta'
+        ]
